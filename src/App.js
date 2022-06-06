0d06b69469098b4208a165c682b77b0df975bb82
Chad Stabler <stablerpsn@gmail.com>
2022-06-06 15:03:03 -0500

    got page to display empty components with name in header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import { zodiac } from './data.js';
-import header from './Header.js';
-import main from './Main.js';
-import footer from './Footer.js';
+import Header from './Header/Header';
+import Main from './Main/Main';
+import Footer from './Footer/Footer';
 {/* import the zodiac array */}
 
 function App() {
   return (
     <div className="App">
+      <Header name="Chad"/>
+      <Main />
+      <Footer />
       {/* import and use the Header. Main, and Footer components here */}
       {/* the Header component needs a string passed in as the `name` prop */}
       {/* the Footer component needs a year passed in as the `year` prop */}
