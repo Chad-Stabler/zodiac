ec6873d6342aec31cd8e0f91da3a2a5fbfb24542
Chad Stabler <stablerpsn@gmail.com>
2022-06-06 17:09:47 -0500

    added class name for zodiac-card style to apply

--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -1,7 +1,7 @@
 import './ZodiacCard.css';
 
 export default function ZodiacCard({ name, dates }) {
-  return (<div>
+  return (<div className='zodiac-card'>
     <p>{name}</p>
     <p>{dates}</p>
     <img src={`/images/${name}.png`}/>
