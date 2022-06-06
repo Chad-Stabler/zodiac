01fee78d914118f230feab22600e09d3ee80737b
Chad Stabler <stablerpsn@gmail.com>
2022-06-06 17:02:56 -0500

    got images to upload correctly

--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -2,8 +2,8 @@ import './ZodiacCard.css';
 
 export default function ZodiacCard({ name, dates }) {
   return (<div>
-    <p>Sign: {name}</p>
-    <p>Dates: {dates}</p>
-    <img src={`url(public/images/${name}.png)`}></img>
+    <p>{name}</p>
+    <p>{dates}</p>
+    <img src={`/images/${name}.png`}/>
   </div>);
 }
